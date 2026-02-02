b6fc5be667cd6d5bedad43b49ef1ee01179142c3
mar <105406641+birdyxs@users.noreply.github.com>
2026-02-02 07:08:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -77,3 +77,24 @@ function openTab(evt, tabName) {
         });
 
         init();
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+							
+document.onkeydown = function (e) {
+							
+if(e.keyCode == 123) {
+return false;
+}
+
+if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
+return false;
+}
+							
+if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+return false;
+}
+
+if(e.ctrlKey && e.keyCode == 85) {
+return false;
+}
+}	
